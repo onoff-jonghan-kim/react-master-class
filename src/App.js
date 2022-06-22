283ac2630b0c3ce0353ae0a6dbf0702a244809f4
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-22 12:33:02 +0900

    styled component as and Attrs

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
 
 const Btn = styled.button`
   color:white;
@@ -25,23 +12,20 @@ const Btn = styled.button`
 
 `;
 
+const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: tomato;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>안좋은 예시</h1>
-      <div style={{display : "flex" }}>
-        <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
-        <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-      </div>
-      <h1>좋은 예시</h1>
-      <Father>
-        <Box bgColor="teal">
-          <Text>Hello!</Text>
-        </Box>
-        <Circle bgColor="tomato"></Circle>
-      </Father>
-    </div>
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
   );
 }
 
