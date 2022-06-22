80d80077bc8b22cfb626696e1860cf92341417f7
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-22 11:35:59 +0900

    first use styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 
+import styled from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
   return (
     <div>
-      test 
+      <h1>안좋은 예시</h1>
+      <div style={{display : "flex" }}>
+        <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+        <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+      </div>
+      <h1>좋은 예시</h1>
+      <Father>
+        <BoxOne><Text>Hello!</Text></BoxOne>
+        <BoxTwo></BoxTwo>
+      </Father>
     </div>
   );
 }
