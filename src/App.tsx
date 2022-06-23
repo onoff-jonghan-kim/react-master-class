c8c8fd2f3b5178cbbf118973f3fe85acf4afa84b
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-23 14:45:34 +0900

    audit err fixed and train interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import styled from "styled-components";
+import Circle from "./Circle";
 
-const Title = styled.h1`
-  color: ${props => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.theme.backgroundColor};
-`;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Some Title</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal"></Circle>
+      <Circle bgColor="tomato"></Circle>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from "styled-components";
 
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-// const darkTheme = {
-//   textColor: "whitesmoke",
-//   backgroundColor: "#111",
-// };
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
