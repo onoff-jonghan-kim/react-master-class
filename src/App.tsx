76602b792f16ae56d80b2c01a5589c82f265724f
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-30 15:51:45 +0900

    typescript react form controll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import styled from "styled-components";
-import Circle from "./Circle";
-
+import React, {useState} from 'react';
+// import styled from "styled-components";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello",value);
+    setValue("");
+  };
   return (
     <div>
-      <Circle bgColor="teal"></Circle>
-      <Circle bgColor="tomato"></Circle>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value}
+          onChange={onChange}
+          type="text" 
+          placeholder="username" 
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
+// import { ThemeProvider } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from "styled-components";
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
-
