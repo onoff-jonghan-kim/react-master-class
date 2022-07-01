b84e2805b791284b8938b9ecf82d52eef76c7b4e
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-01 17:34:33 +0900

    #4.5 fin

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
-
+interface RouteParams {
+  coinId: string;
+}
 const Container = styled.div`
   padding: 0px, 20px;
 `;
@@ -27,7 +29,21 @@ interface RouteState{
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<RouteState>();
-  console.log(state.name);
+  const { coinId } = useParams<RouteParams>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData =  await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
