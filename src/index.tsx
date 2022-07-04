f659c1dcf14dec782f32cd7588b41c0df80d411c
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-04 14:58:53 +0900

    #4.9 fin

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import { ThemeProvider } from "styled-components";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Theme } from './theme';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <div>
-    <ThemeProvider theme={Theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </div>
 );
